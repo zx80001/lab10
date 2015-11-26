c09475419e2a8f5f97ec3cca3f53fea6b376db8d
zx80001 <qwe80001@yahoo.com.hk>
2015-11-26 14:06:24 +0800

    Update server_kitty.js

--- server_kitty.js
+++ server_kitty.js
@@ -4,84 +4,45 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var MONGODBURL = 'mongodb://localhost/test';
 
-var kittySchema = require('./models/kitty');
+var restaurantSchema = require('./models/restaurant');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/kitty/name/:x', function(req,res) {
-	mongoose.connect(MONGODBURL);
-	var db = mongoose.connection;
-	db.on('error', console.error.bind(console, 'connection error:'));
-	db.once('open', function (callback) {
-		var Kitten = mongoose.model('Kitten', kittySchema);
-		//Kitten.find({name: new RegExp(req.params.x)},function(err,results){
-		Kitten.find({name: req.params.x},function(err,results){
-			if (err) {
-				console.log("Error: " + err.message);
-				res.write(err.message);
-			}
-			else {
-				db.close();
-				console.log('Found: ',results.length);
-				res.json(results);
-			}
-		});
-	});
-});
-
-app.get('/kitty/age/:x', function(req,res) {
-	mongoose.connect(MONGODBURL);
-	var db = mongoose.connection;
-	db.on('error', console.error.bind(console, 'connection error:'));
-	db.once('open', function (callback) {
-		var Kitten = mongoose.model('Kitten', kittySchema);
-		//Kitten.find({name: new RegExp(req.params.x)},function(err,results){
-		Kitten.find({age: req.params.x},function(err,results){
-			if (err) {
-				console.log("Error: " + err.message);
-				res.write(err.message);
-			}
-			else {
-				db.close();
-				console.log('Found: ',results.length);
-				res.json(results);
-			}
-		});
-	});
-});
-
-app.get('/kitty/month/:x', function(req,res) {
-	mongoose.connect(MONGODBURL);
-	var db = mongoose.connection;
-	db.on('error', console.error.bind(console, 'connection error:'));
-	db.once('open', function (callback) {
-		var Kitten = mongoose.model('Kitten', kittySchema);
-		//Kitten.find({name: new RegExp(req.params.x)},function(err,results){
-		Kitten.find({'birthday.month': req.params.x},function(err,results){
-			if (err) {
-				console.log("Error: " + err.message);
-				res.write(err.message);
-			}
-			else {
-				db.close();
-				console.log('Found: ',results.length);
-				res.json(results);
-			}
-		});
-	});
-});
-
-app.get('/kitty/birthday/:attrib/:attrib/:attrib_value', function(req,res) {
+app.get('/restaurant/:attrib/:attrib_value', function(req,res) {
+	console.log("find with one attrib");
 	var criteria = {};
-	criteria["birthday."+req.params.attrib] = req.params.attrib_value;
+	criteria[req.params.attrib] = req.params.attrib_value;
 
 	mongoose.connect(MONGODBURL);
 	var db = mongoose.connection;
 	db.on('error', console.error.bind(console, 'connection error:'));
 	db.once('open', function (callback) {
-		var Kitten = mongoose.model('Kitten', kittySchema);
-		Kitten.find(criteria,function(err,results){
+		var Restaurant = mongoose.model('Restaurant', restaurantSchema);
+		Restaurant.find(criteria,function(err,results){
+			if (err) {
+				console.log("Error: " + err.message);
+				res.write(err.message);
+			}
+			else {
+				db.close();
+				console.log('Found: ',results.length);
+				res.json(results);
+			}
+		});
+	});
+});
+app.get('/restaurant/find2/:attrib1/:attrib_value1/:attrib2/:attrib_value2', function(req,res) {
+	console.log("find with 2 attrib");
+	var criteria = {};
+	criteria[req.params.attrib1] = req.params.attrib_value1;
+	criteria[req.params.attrib2] = req.params.attrib_value2;
+	mongoose.connect(MONGODBURL);
+	var db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', function (callback) {
+		var Restaurant = mongoose.model('Restaurant', restaurantSchema);
+		Restaurant.find(criteria,function(err,results){
 			if (err) {
 				console.log("Error: " + err.message);
 				res.write(err.message);
@@ -95,26 +56,91 @@ app.get('/kitty/birthday/:attrib/:attrib/:attrib_value', function(req,res) {
 	});
 });
 
-app.post('/kitty/name', function(req,res) {
+		
+app.get('/restaurant/address/:attrib/:attrib_value', function(req,res) {
+	console.log("find address/")
+	var criteria = {};
+	criteria["address."+req.params.attrib] = req.params.attrib_value;
+
 	mongoose.connect(MONGODBURL);
 	var db = mongoose.connection;
 	db.on('error', console.error.bind(console, 'connection error:'));
 	db.once('open', function (callback) {
-		var Kitten = mongoose.model('Kitten', kittySchema);
-		var k = new Kitten(req.body);
-		k.save(function(err,results){
+		var Restaurant = mongoose.model('Restaurant', restaurantSchema);
+		Restaurant.find(criteria,function(err,results){
 			if (err) {
-				res.end(err.message,500);
+				console.log("Error: " + err.message);
+				res.write(err.message);
 			}
 			else {
 				db.close();
-				res.end('Done',200);
+				console.log('Found: ',results.length);
+				res.json(results);
 			}
 		});
 	});
 });
 
-app.put('/kitty/:name/:attrib/:attrib_value', function(req,res) {
+app.get('/restaurant/address/:coord/:attrib_value1/:attrib_value2', function(req,res) {
+	console.log("find coord with two value")
+	var criteria = [];
+	criteria["address."+req.params.attrib] = req.params.attrib_value1+','+req.params.attrib_value2;
+	//criteria["address."+req.params.attrib] = req.params.attrib_value2;
+	mongoose.connect(MONGODBURL);
+	var db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', function (callback) {
+		var Restaurant = mongoose.model('Restaurant', restaurantSchema);
+		Restaurant.find(criteria,function(err,results){
+			if (err) {
+				console.log("Error: " + err.message);
+				res.write(err.message);
+			}
+			else {
+				db.close();
+				console.log('Found: ',results.length);
+				res.json(results);
+			}
+		});
+	});
+});
+
+app.post('/',function(req,res) {
+	//console.log(req.body);
+	var restaurantSchema = require('./models/restaurant');
+	mongoose.connect('mongodb://localhost/test');
+	var db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', function (callback) {
+		var rObj = {};
+		rObj.address = {};
+		rObj.address.building = req.body.building;
+		rObj.address.street = req.body.street;
+		rObj.address.zipcode = req.body.zipcode;
+		rObj.address.coord = [];
+		rObj.address.coord.push(req.body.lon);
+		rObj.address.coord.push(req.body.lat);
+		rObj.borough = req.body.borough;
+		rObj.cuisine = req.body.cuisine;
+		rObj.name = req.body.name;
+		rObj.restaurant_id = req.body.restaurant_id;
+
+		var Restaurant = mongoose.model('Restaurant', restaurantSchema);
+		var r = new Restaurant(rObj);
+		//console.log(r);
+		r.save(function(err) {
+       		if (err) {
+				res.status(500).json(err);
+				throw err
+			}
+       		//console.log('Restaurant created!')
+       		db.close();
+			res.status(200).json({message: 'insert done', id: r._id});
+    	});
+    });
+});
+
+/*app.put('/kitty/:name/:attrib/:attrib_value', function(req,res) {
 	var criteria = {};
 	criteria[req.params.attrib] = req.params.attrib_value;
 
@@ -137,5 +163,5 @@ app.put('/kitty/:name/:attrib/:attrib_value', function(req,res) {
 		});
 	});
 });
-
+*/
 app.listen(process.env.PORT || 8099);
